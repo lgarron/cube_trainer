20ed127cb15567889a50c6ccc2c0bad4132a36e9
Lucas Garron <code@garron.net>
2021-12-20 21:43:48 -0800

    Update client to pass a string directly to the `TwistyPlayer` constructor.

--- client/src/app/trainer/twisty-player/twisty-player.component.ts
+++ client/src/app/trainer/twisty-player/twisty-player.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TwistyPlayer } from 'cubing/twisty';
-import { parseAlg } from 'cubing/alg';
 import { randomScrambleForEvent } from 'cubing/scramble';
 
 @Component({
   selector: 'app-twisty-player',
   templateUrl: './twisty-player.component.html',
-  styleUrls: ['./twisty-player.component.css']
+  styleUrls: ['./twisty-player.component.css'],
 })
 export class TwistyPlayerComponent implements OnInit {
-
-
   private isValidCubeSize(cubeSize: number) {
     return cubeSize >= 2 && cubeSize <= 7 && Number.isInteger(cubeSize);
   }
@@ -21,23 +18,21 @@ export class TwistyPlayerComponent implements OnInit {
       throw new Error(`invalid cube size ${cubeSize}`);
     }
     const puzzle = `${cubeSize}${cubeSize}${cubeSize}`; // For example 333
-    randomScrambleForEvent(puzzle).then(scramble => console.log(scramble));
+    randomScrambleForEvent(puzzle).then((scramble) => console.log(scramble));
   }
 
-  
   // This is currently in no way useful, it's just here to debug including cubing.js
   createTwistyPlayer() {
     new TwistyPlayer({
-      puzzle: "4x4x4",
-      alg: parseAlg("R U R'"),
-      hintFacelets: "none",
-      backView: "top-right",
-      background: "none"
+      puzzle: '4x4x4',
+      alg: "R U R'",
+      hintFacelets: 'none',
+      backView: 'top-right',
+      background: 'none',
     });
   }
 
   ngOnInit(): void {
     this.randomScramble(3);
   }
-
 }
